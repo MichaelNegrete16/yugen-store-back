b8d02b852498c6a58f901570e3cc68b0266fa120
MichaelNegrete16 <michael.negrete16@gmail.com>
2026-07-10 10:15:46 -0500

    feat: registra OrdersController y ListOrders en PaymentsModule

--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductsModule } from '../products/products.module';
 import { PricingModule } from '../shared/pricing/pricing.module';
 import { GetTransactionUseCase } from './application/get-transaction.usecase';
+import { ListOrdersUseCase } from './application/list-orders.usecase';
 import { ProcessPaymentUseCase } from './application/process-payment.usecase';
 import { PAYMENT_GATEWAY } from './domain/repository/payment-gateway.repository';
 import { TRANSACTION_REPOSITORY } from './domain/repository/transaction.repository';
@@ -12,6 +13,7 @@ import { DeliveryModel } from './infrastructure/models/delivery.model';
 import { TransactionItemModel } from './infrastructure/models/transaction-item.model';
 import { TransactionModel } from './infrastructure/models/transaction.model';
 import { TransactionService } from './infrastructure/services/transaction.service';
+import { OrdersController } from './infrastructure/controllers/orders.controller';
 import { TransactionsController } from './infrastructure/controllers/transactions.controller';
 
 @Module({
@@ -25,10 +27,11 @@ import { TransactionsController } from './infrastructure/controllers/transaction
     ProductsModule,
     PricingModule,
   ],
-  controllers: [TransactionsController],
+  controllers: [TransactionsController, OrdersController],
   providers: [
     ProcessPaymentUseCase,
     GetTransactionUseCase,
+    ListOrdersUseCase,
     { provide: TRANSACTION_REPOSITORY, useClass: TransactionService },
     { provide: PAYMENT_GATEWAY, useClass: PaymentGatewayService },
   ],
